f6546820421f7b58a0b3ac617da0221d25fa9854
Seth DImmer <sethdimmer1@gmail.com>
2023-01-11 05:01:52 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,13 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 function App() {
   function register() {
     console.log('register')
+    createUserWithEmailAndPassword(auth, 'email@email.com', 'test123')
+    .then((user) => {
+      console.log(user)
+    })
+    .catch((error) => {
+      console.log(error);
+    })
   }
   return (
     <div className="App">
